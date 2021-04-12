7aa9771b32ce4a1958accc9ff0f2965e23dbd7bf
qizhengyaoyao <28369996+qizhengyaoyao@users.noreply.github.com>
2021-04-12 21:30:43 +1000

    api_key problem solved

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ from flask import (
     redirect)
 import pymongo
 from flask_cors import CORS
+import config
 
 #################################################
 # Database Setup
 #################################################
-url = "mongodb+srv://mahjong:mahjong@cluster0.pyqix.mongodb.net/vic_crime?retryWrites=true&w=majority"
+client = pymongo.MongoClient(os.getenv("MONGODB_URI", config.url))
 
-client = pymongo.MongoClient(os.getenv("MONGODB_URI", url))
+API_KEY=os.getenv("API_KEY", config.API_KEY)
 
 vic_db = client['vic_crime']
 
@@ -41,7 +42,7 @@ def data():
 
 @app.route("/visualisation")
 def visualisation():
-    return render_template("visualisation.html")
+    return render_template("visualisation.html", API_KEY=API_KEY)
 
 @app.route("/api_doc")
 def welcome():
