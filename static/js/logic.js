8c477129059707edecf8b57312ac6d630c0542d6
Tom Peddlesden <tpeddlesden@gmail.com>
2021-04-14 14:43:17 +1000

    added suburbs to bindPopup for LGA selected

--- static/js/logic.js
+++ static/js/logic.js
@@ -23,13 +23,14 @@ var geojson; // empty variable to hold cloropleth
 
 const lgaCrimeData = "/api/v3.0/lga/all?off_field=subdiv";
 const crimeTypes = "/api/v3.0/all_type";
+const suburbsLga = "../static/data/suburbs_lga.json"
 
 const suburbCrimeData = "";
 
 function getDataAddMarkers({ label, value, map }) {
       console.log(`Timeline slider is set to ${parseInt(label)}`);
 
-//======================================= CLEARING CLOROPLETH / LEGEND ====================================
+      //======================================= CLEARING CLOROPLETH / LEGEND ====================================
 
       // Clear the choropleth layer at the start of every timeline slider change (including init)
       map.eachLayer(function (layer) {
@@ -39,9 +40,9 @@ function getDataAddMarkers({ label, value, map }) {
       })
 
       var legendRemove = d3.select(".legend");
-            legendRemove.remove();
+      legendRemove.remove();
 
-//=========================================== FILTERING ==================================================
+      //=========================================== FILTERING ==================================================
 
       // Grabbing our GeoJSON data..
       d3.json(lgaAPI, function (data) {
@@ -53,7 +54,7 @@ function getDataAddMarkers({ label, value, map }) {
                   var crimeDivisions = {};
                   var crimeJSON = cData[label];
 
-                   // Store the offencecrime division code (e.g. A, B, C etc.)
+                  // Store the offencecrime division code (e.g. A, B, C etc.)
                   // and the offence/crime division into the variable crimeDivisions
                   // for use in the function getLGACrime that will populate the popup
                   d3.json(crimeTypes, function (cTypes) {
@@ -63,17 +64,17 @@ function getDataAddMarkers({ label, value, map }) {
                               if (!crimeDivisions[cTypes[i]["Offence Division code"]]) {
                                     crimeDivisions[cTypes[i]["Offence Division code"]] = cTypes[i]["Offence Division"];
                               }
-                               else {
+                              else {
                                     continue;
                               }
                         }
-                        console.log(crimeDivisions);
-                        
+                        // console.log(crimeDivisions);
+
                         var valueCrime = [];
                         for (let i = 0; i < data.features.length; i++) {
                               var lgaProperties = data.features[i].properties;
                               //console.log(lgaName);
-                                    
+
                               for (let lga in crimeJSON) {
                                     if (lgaProperties.ABB_NAME == lga.toUpperCase()) {
                                           // console.log(lgaJSON.CRIME_TOTAL);
@@ -85,17 +86,17 @@ function getDataAddMarkers({ label, value, map }) {
                                     }
                                     else {
                                           continue;
-                                    }                      
-                              };      
+                                    }
+                              };
                         };
 
-//=========================================== LEGEND ===================================================
+                        //=========================================== LEGEND ===================================================
 
-                        sortedCrimeValues = valueCrime.sort((a, b) => b - a);   
+                        sortedCrimeValues = valueCrime.sort((a, b) => b - a);
                         var maxCrime = sortedCrimeValues[0]
                         var minCrime = sortedCrimeValues[sortedCrimeValues.length - 1]
-                        var steps = (maxCrime - minCrime)/10
-                        
+                        var steps = (maxCrime - minCrime) / 10
+
                         //https://gka.github.io/palettes/
                         var colours = ['#00ff66', '#62ed60', '#85da5a', '#9cc754', '#adb34f', '#bb9f49', '#c68944', '#cf723f', '#d6573a', '#dc3136']
 
@@ -108,22 +109,22 @@ function getDataAddMarkers({ label, value, map }) {
                               div.innerHTML += '<span><b>Year: ' + label + ' </b></span><br>'
                               div.innerHTML += "<h9><b>Highest Crime</b></h9><br>";
                               div.innerHTML += '<i style="background:' + colours[9] + '"></i><span><b>' + maxCrime + ' -</b></span><br>';
-                              div.innerHTML += '<i style="background:' + colours[8]  + '"></i><span></span><br>';
-                              div.innerHTML += '<i style="background:' + colours[7]  + '"></i><span></span><br>';
-                              div.innerHTML += '<i style="background:' + colours[6]  + '"></i><span></span><br>';
-                              div.innerHTML += '<i style="background:' + colours[5]  + '"></i><span></span><br>';
-                              div.innerHTML += '<i style="background:' + colours[4]  + '"></i><span></span><br>';
-                              div.innerHTML += '<i style="background:' + colours[3]  + '"></i><span></span><br>';
-                              div.innerHTML += '<i style="background:' + colours[2]  + '"></i><span></span><br>';
-                              div.innerHTML += '<i style="background:' + colours[1]  + '"></i><span></span><br>';
-                              div.innerHTML += '<i style="background:' + colours[0]  + '"></i><span><b>' + minCrime + ' -</b></span><br>';
+                              div.innerHTML += '<i style="background:' + colours[8] + '"></i><span></span><br>';
+                              div.innerHTML += '<i style="background:' + colours[7] + '"></i><span></span><br>';
+                              div.innerHTML += '<i style="background:' + colours[6] + '"></i><span></span><br>';
+                              div.innerHTML += '<i style="background:' + colours[5] + '"></i><span></span><br>';
+                              div.innerHTML += '<i style="background:' + colours[4] + '"></i><span></span><br>';
+                              div.innerHTML += '<i style="background:' + colours[3] + '"></i><span></span><br>';
+                              div.innerHTML += '<i style="background:' + colours[2] + '"></i><span></span><br>';
+                              div.innerHTML += '<i style="background:' + colours[1] + '"></i><span></span><br>';
+                              div.innerHTML += '<i style="background:' + colours[0] + '"></i><span><b>' + minCrime + ' -</b></span><br>';
                               div.innerHTML += '<h8><b>Lowest Crime</b></h9>'
 
                               return div;
                         };
                         legend.addTo(myMap);
 
-//======================================= CRIME DIVISION VALUES PER LGA =======================================
+                        //====================================== FUNCTION TO GET CRIME DIVISION VALUES PER LGA =======================================
 
                         // Populate the popup with relevant crime division data to each LGA
                         function getLGACrime(lgaName) {
@@ -131,76 +132,100 @@ function getDataAddMarkers({ label, value, map }) {
                                     var filteredLGA = Object.entries(cData[label][lgaName]).filter((result, i) => result);
                                     var output = [];
                                     Object.entries(filteredLGA[3][1].Div).forEach((key, value) => {
-                                          output.push(`<b>${crimeDivisions[key[0]]}: </b>:${key[1]}<br>`);
+                                          output.push(`<b>${crimeDivisions[key[0]]}: </b>${key[1]}<br>`);
                                     })
                                     console.log(key[1])
                               } catch (err) { // catch any errors from a lack of data, etc.
-                                    console.log(`no data for ${lgaName}`);
-                                 
+                                    // console.log(`no data for ${lgaName}`);
+
                               }
                               return output;
                         };
 
-//=================================================CLOROPLETH =============================================
+                        //========================================= FUNCTION TO GET SUBURB NAMES FROM LGA ======================================
+                        d3.json(suburbsLga, function (suburbsData) {
 
-                        // Create a new choropleth layer
-                        geojson = L.choropleth(data, {
-                              // Define what  property in the features to use
-                              valueProperty: "CRIME_TOTAL",
-                              // Set color scale
-                              scale: [colours[0],colours[9]],
-                              // Number of breaks in step range
-                              steps: 10,
-                              // q for quartile, e for equidistant, k for k-means
-                              mode: "q",
-                              style: {
-                                    // Border color
-                                    color: "#fff",
-                                    weight: 1,
-                                    fillOpacity: 0.8
-                              },
+                              function getSuburbs(lga) {
 
-                              // Binding a pop-up to each layer
-                              onEachFeature: function (feature, layer) {
-                                    // Set mouse events to change map styling
-                                    layer.on({
-                                          // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-                                          mouseover: function (event) {
-                                                layer = event.target;
-                                                layer.setStyle({
-                                                      fillOpacity: 1
-                                                });
-                                          },
-                                          // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-                                          mouseout: function (event) {
-                                                layer = event.target;
-                                                layer.setStyle({
-                                                      fillOpacity: 0.8
-                                                });
-                                          },
-                                          // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
-                                          click: function (event) {
-                                                myMap.fitBounds(event.target.getBounds());
+                                    var suburbs = [];
+                                    for (let i = 0; i < suburbsData.length; i++) {
+                                          if (lga == suburbsData[i].lga_name) {
+                                                suburbs.push(` ${suburbsData[i].suburb.toUpperCase()}`)
                                           }
-                                    });
-                                    // call getLGACrime function, parse LGA Name in capitalised format to match our lgaCrimeData json
-                                    layer.bindPopup(`<h4><b> ${feature.properties.ABB_NAME} </b></h4>
-                                    <hr>
-                                          
-                                          ${getLGACrime(feature.properties.ABB_NAME.trim().toLowerCase().replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase()))))}`                                      
-                                    );
-                              }
-                        }).addTo(myMap)
+                                          else {
+                                                continue;
+                                          };
+                                    };
+                                    return suburbs;
+
+                              };
+                              // console.log(getSuburbs("banyule"))
+
+                              //================================================= CLOROPLETH =============================================
+
+                              // Create a new choropleth layer
+                              geojson = L.choropleth(data, {
+                                    // Define what  property in the features to use
+                                    valueProperty: "CRIME_TOTAL",
+                                    // Set color scale
+                                    scale: [colours[0], colours[9]],
+                                    // Number of breaks in step range
+                                    steps: 10,
+                                    // q for quartile, e for equidistant, k for k-means
+                                    mode: "q",
+                                    style: {
+                                          // Border color
+                                          color: "#fff",
+                                          weight: 1,
+                                          fillOpacity: 0.8
+                                    },
+
+                                    // Binding a pop-up to each layer
+                                    onEachFeature: function (feature, layer) {
+                                          // Set mouse events to change map styling
+                                          layer.on({
+                                                // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+                                                mouseover: function (event) {
+                                                      layer = event.target;
+                                                      layer.setStyle({
+                                                            fillOpacity: 1
+                                                      });
+                                                },
+                                                // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+                                                mouseout: function (event) {
+                                                      layer = event.target;
+                                                      layer.setStyle({
+                                                            fillOpacity: 0.8
+                                                      });
+                                                },
+                                                // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+                                                click: function (event) {
+                                                      myMap.fitBounds(event.target.getBounds());
+                                                }
+                                          });
+                                          // call getLGACrime function, parse LGA Name in capitalised format to match our lgaCrimeData json
+                                          layer.bindPopup(`<h4><b> ${feature.properties.ABB_NAME} </b></h4>
+                                          <hr>
+                                          <b>Crimes:</b> 
+                                          ${getLGACrime(feature.properties.ABB_NAME.trim().toLowerCase().replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase()))))}      
+                                          <hr>
+                                          <b>Suburbs:</b>
+                                          ${getSuburbs(feature.properties.ABB_NAME.toLowerCase())}
+                                          `
+                                          );
+                                    }
+
+                              }).addTo(myMap)
+                        });
                   });
             });
       });
 };
 
 
+
 //================================================ Timeline Slider ==========================================
-
-
-// Custom function to change the map per our API calls
+// Custom function to change the map per our API calls  ${getLgaSuburbs(feature.properties.ABB_NAME.toLowerCase())}
 // Timeline slider plugin for leaflet, can be placed within a function
 // Refer to docs: https://github.com/svitkin/leaflet-timeline-slider
 L.control.timelineSlider({
